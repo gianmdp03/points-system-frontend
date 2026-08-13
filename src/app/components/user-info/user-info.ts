c9fde8253f61a88da9ae7afff76d895454ac3f32
Joaquin <joaco.rodriguezortega@gmail.com>
2026-08-13 17:33:56 -0300

    .

--- src/app/components/user-info/user-info.ts
+++ src/app/components/user-info/user-info.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { AppConfigService } from '../../core/services/app-config-service';
 
 @Component({
   selector: 'app-user-info',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user-info.html',
   styleUrl: './user-info.css',
 })
